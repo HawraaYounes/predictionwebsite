995cc2415ae90cab5888e23fb849b62d1532a503
HawraaYounes <hawraayounes0@gmail.com>
2022-09-10 11:56:13 +0300

    Renaming variables and adding styles

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 var dogImage = document.getElementById('dog-img');
-var name_input = document.getElementById('name_input');
-var my_name = document.getElementById('name');
+var nameInput = document.getElementById('name_input');
+var myName = document.getElementById('name');
 var gender = document.getElementById('gender');
 var age = document.getElementById('age');
 var nationality = document.getElementById('nationality');
 var btn = document.getElementById('btn');
-var name_value;
+var nameValue;
 
-console.log("helllo");
 window.addEventListener('load', fetchDogImage);//Display random dog image once the page is loaded.
 btn.addEventListener('click', fetchApis);//Once the button is clicked, the function fetchApis() is called
 
@@ -21,7 +20,7 @@ function fetchDogImage() {//function that fetch dog images api, and add it to th
 }
 
 function fetchGender(){//function that fetch predicted gender api, and add it to the page
-  fetch('https://api.genderize.io?name=rio')
+  fetch('https://api.genderize.io?name='+nameValue)
     .then(response => response.json())
     .then(data => {
       gender.innerHTML = `<p>"${data.gender}"</p>`;
@@ -29,7 +28,7 @@ function fetchGender(){//function that fetch predicted gender api, and add it to
 }
 
 function fetchAge(){//function that fetch age api, and add it to the page
-  fetch('https://api.agify.io/?name=nour')
+  fetch('https://api.agify.io/?name='+nameValue)
     .then(response => response.json())
     .then(data => {
       age.innerHTML = `<p>I'm "${data.age}" years old.</p>`;
@@ -37,25 +36,26 @@ function fetchAge(){//function that fetch age api, and add it to the page
 }
 
 function  fetchNationality(){//function that nationality api, and add it to the page
-  fetch('https://api.nationalize.io/?name=mohamad')
+  fetch('https://api.nationalize.io/?name='+nameValue)
     .then(response => response.json())
     .then(data => {
-      for(let i=0;i<data.country.length;i++){
+      for(let i=0;i<2;i++){
         nationality.innerHTML += `<p>"${data.country[i].country_id}"</p>`;
     }
   });
 }
 
 function fetchApis(){//function that calls the three previous functions and dispalys the name
-    if (name_input.value== "") {
-        my_name.innerHTML="Enter your name first!";
+    if (nameInput.value== "") {
+        myName.innerHTML="Enter your name first!";
+        
      }
      else{
-        console.log("hiiiii");
-        name_value = name_input.value;
-        my_name.innerHTML=name_value;
+        nameValue = nameInput.value;
+        myName.innerHTML=nameValue;
         fetchGender();
         fetchAge();
         fetchNationality();
+
      }
 }
